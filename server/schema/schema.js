8b6071aea05df57ef8e44a0e5d89d5dd9c438456
Sean Hong <iamseanhong@gmail.com>
2018-05-19 16:31:33 -0700

    Set up first graphql schema 'Book'

---
+++ server/schema/schema.js
@@ -0,0 +1,10 @@
+const { GraphQLObjectType, GraphQLString } = require('graphql');
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
+});
