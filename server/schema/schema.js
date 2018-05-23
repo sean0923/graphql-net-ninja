14aa053d88a4b3416734e0bf3285c35cca0c8bf8
Sean Hong <iamseanhong@gmail.com>
2018-05-23 13:30:02 -0600

    Write first mutation 'addAuthor'

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,8 +11,8 @@ const _ = require('lodash');
 const faker = require('faker');
 
 // mongoose model
-const Book = require('./models/book');
-const Author = require('./models/author');
+const Book = require('../models/book');
+const Author = require('../models/author');
 
 // fake data -----------------------------------------------------------------
 const fakeBookData = [
@@ -100,6 +100,28 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addAuthor: {
+      type: AuthorType,
+      args: {
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+      },
+      resolve(parent, args) {
+        let author = new Author({
+          name: args.name,
+          age: args.age,
+        });
+
+        return author.save();
+      },
+    },
+  },
+});
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation: Mutation,
 });
